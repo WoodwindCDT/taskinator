2fb308c0a1adf70a379c63093c5ac2c22f0aad46
Ender <woodwind.turbeville@gmail.com>
2020-08-19 11:34:28 -0500

    Added var(s) and adjusted function to allow user inputs to be created into seperate listed items in form

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // To allow an event on click and add to HTML 'MAIN, UL, LI'
 var createTaskHandler = function() {
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     event.preventDefault();
+
+    // To create a list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // To create a div to hold the task info and add a li
+    var taskInfoEl = document.createElement("div");
+    // Giving ^ a class name
+    taskInfoEl.className = "task-info";
+    // To add HTML content within the div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // To add entire li to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
