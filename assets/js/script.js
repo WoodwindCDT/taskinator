02fc873d04b985cc88fa8fe01277070efc3f594e
Ender <woodwind.turbeville@gmail.com>
2020-08-18 19:00:29 -0500

    Fixed Syntax

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ var createTaskHandler = function() {
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 // To allow the event to run function 'createTaskHandler'
 formEl.addEventListener("submit", createTaskHandler);
