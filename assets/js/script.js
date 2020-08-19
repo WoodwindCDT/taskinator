4678856c1aa7a390091de3c8c82aa0d822d8a17a
Ender <woodwind.turbeville@gmail.com>
2020-08-19 12:15:50 -0500

    Simplified functionlality within JS for easier implemintation for future progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,38 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // To allow an event on click and add to HTML 'MAIN, UL, LI'
-var createTaskHandler = function() {
+var taskFormHandler  = function() {
+    event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    event.preventDefault();
 
-    // To create a list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
+    // To compile data into an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
-    // To create a div to hold the task info and add a li
-    var taskInfoEl = document.createElement("div");
-    // Giving ^ a class name
-    taskInfoEl.className = "task-info";
-    // To add HTML content within the div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    // To attach it and send to createTaskEl (argument)
+    createTaskEl(taskDataObj);
+};
 
-    listItemEl.appendChild(taskInfoEl);
+var createTaskEl = function(taskDataObj) {
+   // To create a list item
+   var listItemEl = document.createElement("li");
+   listItemEl.className = "task-item";
 
-    // To add entire li to list
-    tasksToDoEl.appendChild(listItemEl);
+   // To create a div to hold the task info and add a li
+   var taskInfoEl = document.createElement("div");
+   // Giving ^ a class name
+   taskInfoEl.className = "task-info";
+   // To add HTML content within the div
+   taskInfoEl.innerHTML = "<h3 class='task-name'>" +taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+   listItemEl.appendChild(taskInfoEl);
+
+   // To add entire li to list
+   tasksToDoEl.appendChild(listItemEl);
 };
 
 // To allow the event to run function 'createTaskHandler'
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
